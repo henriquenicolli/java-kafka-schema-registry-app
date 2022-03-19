3117e65eea9f98915edb65edde66721ac77351c1
Henrique Nicolli <henrique.nicolli@gmail.com>
2022-03-19 19:32:00 -0300

    first commit

---
+++ kafka_broker_producer/src/test/java/com/kafka/broker/example/kafka_broker/KafkaBrokerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafka.broker.example.kafka_broker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaBrokerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
